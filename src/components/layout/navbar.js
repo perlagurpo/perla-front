d29ea4ab78e57950a5bf2d84d25c041dff121768
Numa-diazdiva <manuel.diazd@hotmail.com>
2023-10-16 16:09:54 -0300

    Dispatch en navbar y armado de menú de config

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -3,27 +3,85 @@ import { useState } from 'react';
 import Link from "next/link";
 import { useSelector, useDispatch } from 'react-redux';
 import { setLang } from '@/app/store/features/langSlice';
+import { setLite } from '@/app/store/features/liteSlice';
 import { GearIcon } from '../icons/icons';
 
 export default function Navbar() {
   /* Estado local de la UI */
   const [menuOpened, setMenuOpened] = useState(false);
   const [langMenuOpened, setLangMenuOpened] = useState(false);
+  const [langMenuActive, setLangMenuActive] = useState(false);
   const [configMenuOpened, setConfigMenuOpened] = useState(false);
+  const [configMenuOpacity, setConfigMenuOpacity] = useState(false);
   /* Manejo de estado de Redux */
   const scroll = useSelector((state) => state.scroll.value);
   const lang = useSelector((state) => state.lang.value);
   const availableLangs = useSelector((state) => state.lang.availableValues);
   const content = useSelector((state) => state.lang.content);
+  const liteStatus = useSelector((state) => state.lite.value );
   const dispatch = useDispatch();
 
 
   function toggleConfigMenu() {
-    set
+    displayLangMenu(false);
+    if (!configMenuOpened) {
+      setConfigMenuOpened(true);
+      setTimeout(() => setConfigMenuOpacity(1), 100);
+    } else {
+      setConfigMenuOpacity(0);
+      setTimeout(() => setConfigMenuOpened(false), 300);
+    }
+  }
+
+  function displayLangMenu(value) {
+    if (value) {
+      setLangMenuOpened(true);
+      setTimeout(() => setLangMenuActive(true), 100);
+    } else {
+      setLangMenuActive(false);
+      setTimeout(() => setLangMenuOpened(false), 100);
+    }
+  }
+
+  /**
+   * Renderiza el botón de cambio de lenguaje
+   * @returns JSX Element
+   */
+  function LangUI() {
+    return(
+        <div className='relative h-full'>
+          <button onClick={ () => {displayLangMenu(!langMenuOpened)} }>
+            <h2 className='font-[made-tommy-thin] hover:opacity-80 transition duration-100'>
+              { lang }
+            </h2>
+          </button>
+          {/* Menú de lenguajes disponibles */}
+          <div  className={`absolute ${ langMenuOpened ? 'flex' : 'hidden'}
+                            z-20 min-w-md bg-[#505050]
+                            rounded-lg shadow-lg
+                            translate-y-3 translate-x-[-25%]
+                            opacity-[${langMenuActive ? "1" : "0"}] transition duration-300 delay-100`}
+                onClick={() => displayLangMenu(false)}
+                >
+                <div className={`flex flex-row items-center gap-2 px-3 py-2`}>
+                  { 
+                    availableLangs && 
+                      availableLangs.map( (data, i) => {
+                        return  <h2 className='font-[made-tommy-thin] text-sm cursor-pointer hover:text-perla-primary transition duration-100'
+                                    onClick={() => {dispatch(setLang(data))}}
+                                    key={i}>
+                                  { data }
+                                </h2>
+                      })
+                  }
+                </div>
+          </div>
+        </div>
+    );
   }
 
   return(
-    <nav className={`sticky flex flex-col bg-black font-['made-tommy-regular'] items-center z-10 transition duration-500`} style={{ 'backgroundColor' : `rgba(0, 0, 0, ${ scroll > 0.02 ? '0' : '1' }`, transition: 'all 1s ease' }}>
+    <nav className={`sticky flex flex-col z-10 items-center bg-black font-['made-tommy-regular'] transition duration-500`} style={{ 'backgroundColor' : `rgba(0, 0, 0, ${ scroll > 0.02 ? '0' : '1' }`}}>
       <div className="min-w-full flex flex-wrap items-center justify-between py-2 md:px-[5em] lg:px-[12em]">
         <Link href="/" className="flex items-center">
           <img src="/img/perla.png" className="h-10 px-6" alt="Perla Labs" />
@@ -35,7 +93,7 @@ export default function Navbar() {
           <svg className="w-6 h-6 fill-perla-primary" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
         </button>
         <div className="flex flex-row hidden w-full h-full md:block md:w-auto" id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 md:p-0 md:flex-row md:gap-x-[6em] md:mt-0 md:border-0 font-semibold items-center">
+          <ul className={`flex flex-col items-center p-4 mt-4 md:p-0 md:flex-row md:gap-x-[6em] md:mt-0 md:border-0 font-semibold ${scroll > 0.02 && "text-black"} transition duration-500`}>
             <li>
               <Link href="/" className="block py-2 pl-3 pr-4 rounded md:border-0 md:hover:text-perla-primary md:p-0 transition duration-500">
                 { content["navbar"].about }
@@ -47,42 +105,31 @@ export default function Navbar() {
               </Link>
             </li>
             <li>
-              <Link href="/contact" className="block py-2 pl-3 pr-4 rounded md:border-0 md:hover:text-perla-primary md:p-0 transition duration-500">
+              <Link href="/contact" className="block py-2 pl-3 pr-4 rounded md:border-0 md:hover:text-perla-primary md:p-0 transition duration-300">
                 { content["navbar"].contact }
               </Link>
             </li>
-            <li className='relative h-full'>
-              <button onClick={ () => {setLangMenuOpened(!langMenuOpened)} }>
-                <h2 className='font-[made-tommy-thin]'>
-                  { lang }
-                </h2>
-              </button>
-              <div  className={`flex flex-row items-center absolute ${ langMenuOpened ? "translate-y-0" : "translate-y-4"} gap-2 z-20 bg-perla-black px-3 py-2 rounded-lg  shadow-lg ${ langMenuOpened ? 'block' : 'hidden'}  transition-transform duration-500`}
-                    onClick={() => setLangMenuOpened(false)}
+                                
+       
+            <li className="flex flex-row items-center gap-2 tracking-wide">
+              {/* Menú de configuración */}
+              <div className={`${configMenuOpened ? "flex flex-row" : "hidden" }
+                                items-center min-w-md gap-6 rounded-xl
+                                bg-perla-black text-white
+                                mx-4 px-4 py-2
+                                opacity-${configMenuOpacity}
+                                ${configMenuOpacity == 1 ? "translate-x-0" : "translate-x-12"}
+                                transition duration-300`}>
+                <LangUI />
+                <p  className={`text-ms font-[made-tommy-thin] ${liteStatus && "text-perla-primary"} cursor-pointer hover:opacity-80 transition duration-100`}
+                    onClick={() => {dispatch(setLite(!liteStatus))}}
                     >
-                      { 
-                        availableLangs && 
-                          availableLangs.map( (data, i) => {
-                            return  <h2 className='font-[made-tommy-thin] text-sm cursor-pointer hover:text-perla-primary transition duration-100'
-                                        onClick={() => {dispatch(setLang(data))}}
-                                        key={i}>
-                                      { data }
-                                    </h2>
-                          })
-                      }
-              </div>
-              
-            </li>
-            <li className="flex flex-row items-center">
-              <div className={`${configMenuOpened ? "flex" : "hidden" } px-4 min-w-md rounded-lg bg-black mx-4 py-2 ${configMenuOpened ? "opacity-100" : "opacity-0"} transition duration-300`}>
-                <p className="text-sm cursor-pointer">
                   lite
                 </p>
               </div>
-              <div onClick={() => setConfigMenuOpened(!configMenuOpened)} className="cursor-pointer">
+              <div onClick={() => toggleConfigMenu()} className={`cursor-pointer ${configMenuOpened && "rotate-90"} hover:opacity-80 transition duration-300`}>
                 <GearIcon width={'20px'} height={'20px'} />
               </div>
-              
             </li>
           </ul>
           
@@ -92,18 +139,18 @@ export default function Navbar() {
           <ul className="flex flex-col text-center p-4 text-lg font-medium">
             <li>
               <Link href="/" className="block py-2 pl-3 pr-4 rounded md:border-0 md:p-0">
-                Eventos
+                { content["navbar"].projects }
               </Link>
             </li>
             
             <li>
               <Link href="/" className="block py-2 pl-3 pr-4 rounded md:border-0 md:p-0">
-                Nosotros
+                { content["navbar"].about }
               </Link>
             </li>
             <li>
               <Link href="/contact" className="block py-2 pl-3 pr-4 rounded md:border-0 md:p-0">
-                Contacto
+              { content["navbar"].contact }
               </Link>
             </li>
           </ul>
