2f06e1811f7b374de59862a6a6b1f87a8dc85ba4
Numa-diazdiva <manuel.diazd@hotmail.com>
2023-12-01 18:59:49 -0300

    Responsive choreado del original

--- src/components/home/three/landingScene/pages/pearl-reducida.js
+++ src/components/home/three/landingScene/pages/pearl-reducida.js
@@ -1,22 +1,38 @@
 import { useRef, useState, useEffect } from 'react';
 import { useFrame, useThree } from '@react-three/fiber';
 import { Float, PresentationControls, Text, useGLTF, useScroll } from '@react-three/drei';
+import { useSpring, animated } from '@react-spring/three';
 import { damp } from 'three/src/math/MathUtils';
 import { VideoTexto } from '@/components/utils/three/videoText';
 
 function PearlSceneReducida({ text, active }) {
   const scroll = useScroll();
+  const { viewport } = useThree();
   const pearl = useGLTF('/models/pearl/source/pearl3.gltf');
   const pearlRef = useRef();
   const modelGroupRef = useRef();
   const titleRef = useRef();
   const subtitleRef = useRef();
   const [visible, setVisible] = useState(true);
+  const [vertical, setVertical] = useState(false);
   const descriptionFontProps = { font: '/fonts/made_tommy_soft/mt_Soft_Thin_PERSONAL_USE.otf', fontSize: 1, letterSpacing: 0.04, lineHeight: 1.2, 'material-toneMapped': false }
   
   var prevScroll = scroll.scroll.current;
 
-  const { viewport } = useThree();
+  
+  // Posiciones responsive
+  const titleSpring = useSpring({
+    position:  [viewport.width > 13 ? 2 : 0,
+                active ? ((viewport.width > 13 ? 4 : 6)): 20,
+                4],
+    config : {
+      friction: 100,
+      mass: 20,
+    }
+  });
+
+  // Vertical para responsive
+  useEffect(() => { viewport.width < 11 ? setVertical(true) : setVertical(false) },[viewport.width]);
 
   useFrame(
     (state, delta) => {
@@ -49,7 +65,7 @@ function PearlSceneReducida({ text, active }) {
   return(
     <group visible={visible}>
       {/* <OrbitControls /> */}
-      <group ref={modelGroupRef} scale={ viewport.width / 18 }>
+      <group ref={modelGroupRef} scale={ viewport.width / (viewport.width > 13 ? 18 : 10)}>
         <directionalLight color="blue" position={[0, 10, 0]} intensity={0.31}/>
         <PresentationControls polar={[0, 0]} speed={10} config={{ mass: 0.1, tension: 170, friction: 26 }} >
           <Float rotationIntensity={0} floatIntensity={1.5} floatingRange={[0, 1]}>
@@ -62,12 +78,16 @@ function PearlSceneReducida({ text, active }) {
           </Float>
         </PresentationControls>
       </group>
-
-      <group ref={titleRef} position={[2,4,4]} scale={ viewport.width / 25 }>
-        <VideoTexto texto={"Perla software"} videoSource={'/video/naturaleza.mp4'} fontSize={2.6} />
-      </group>
       
-      <Text position={[0,2,6]} scale={0.5} color="black" ref={subtitleRef} {...descriptionFontProps}>
+      <animated.group ref={titleRef}
+                      position={titleSpring.position}
+                      scale={ viewport.width / (viewport.width > 13 ? 25 : (vertical ? 15 : 18))  }
+                      >
+        <VideoTexto texto={`Perla${ vertical ? ""  :" software"}`} videoSource={'/video/naturaleza.mp4'} fontSize={viewport.width < 10 ? 4 : 2.6} />
+        { vertical && <group position={[2,-2.5,0]}><VideoTexto texto={"software"} videoSource={'/video/naturaleza.mp4'} fontSize={2.6} /></group> }
+      </animated.group>
+      
+      <Text position={[0,2,6]} scale={vertical ? 0.35 : 0.5} color="black" ref={subtitleRef} {...descriptionFontProps}>
         {"en construcción"}
       </Text>
     </group>
